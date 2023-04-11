f9524f4de9f3386bd5c28e197f6b17322735577f
aronaacho <114690424+aronaacho@users.noreply.github.com>
2023-04-11 15:52:30 -0700

    Update Q4

--- question-4.sql
+++ question-4.sql
@@ -1,5 +1,5 @@
 -- Which sales agent made the most in sales in 2010?
-SELECT employees.FirstName, employees.LastName FROM employees LEFT OUTER JOIN customers ON employees.EmployeeId = customers.SupportRepId LEFT OUTER JOIN invoices ON customers.CustomerId = invoices.CustomerId WHERE invoiceDate LIKE '2010%' GROUP BY employees.FirstName ORDER BY COUNT(employees.FirstName) DESC LIMIT 1;
+SELECT employees.FirstName, employees.LastName FROM employees LEFT OUTER JOIN customers ON employees.EmployeeId = customers.SupportRepId LEFT OUTER JOIN invoices ON customers.CustomerId = invoices.CustomerId LEFT OUTER JOIN invoice_items ON invoices.InvoiceId = invoice_items.InvoiceId WHERE invoiceDate LIKE '2010%' GROUP BY employees.FirstName ORDER BY SUM(invoice_items.UnitPrice) DESC LIMIT 1;
 /*
 link to referenced code (ORDER BY COUNT() & LIMIT):
 https://stackoverflow.com/questions/12235595/find-most-frequent-value-in-sql-column
